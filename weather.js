06e7a8cad0ce0057b2b06a13a8c32ace7e3d0f7a
Xuewen Yang <xuewenyang1@gmail.com>
2023-12-10 00:52:20 -0800

    Weather widge completed

--- weather.js
+++ weather.js
@@ -6,6 +6,7 @@ class WeatherWidget extends HTMLElement {
 
         fetch(apiUrl).then((response) => response.json())
             .then((data) => {
+                console.log(data);
                 const curData = data.properties.periods[0];
                 const temp = curData.temperature;
                 const unit = curData.temperatureUnit;
@@ -14,8 +15,40 @@ class WeatherWidget extends HTMLElement {
                 const shortForecast = curData.shortForecast;
                 const curShortForecast = `${shortForecast}`;
 
+                const weatherIcon = document.createElement('img');
+                
+                const forecastWords = shortForecast.split(' ');
+                const weather = ['rainy', 'sunny', 'cloudy', 'foggy', 'snowy', 'windy'];
+                const curWeather = forecastWords.find(w => weather.includes(w.toLowerCase()));
+                if (curWeather === 'rainy') {
+                    weatherIcon.src = "weather-icons/rainy.png";
+                    weatherIcon.alt = "rainy";
+                } else if (curWeather === 'sunny') {
+                    weatherIcon.src = "weather-icons/sunny.png";
+                    weatherIcon.alt = "sunny";
+                } else if (curWeather === 'cloudy') {
+                    weatherIcon.src = "weather-icons/cloudy.png";
+                    weatherIcon.alt = "cloudy";
+                } else if (curWeather === 'foggy') {
+                    weatherIcon.src = "weather-icons/foggy.png";
+                    weatherIcon.alt = "foggy";
+                } else if (curWeather === 'snowy') {
+                    weatherIcon.src = "weather-icons/snowy.png";
+                    weatherIcon.alt = "snowy";
+                } else if (curWeather === 'windy') {
+                    weatherIcon.src = "weather-icons/windy.png";
+                    weatherIcon.alt = "windy";
+                } else {
+                    weatherIcon.src = "weather-icons/clear.png";
+                    weatherIcon.alt = "clear";
+                }
+                
                 const relativeHumidity = curData.relativeHumidity.value;
                 const curHumidity = `${relativeHumidity} %`;
+
+                const windSpeed = curData.windSpeed;
+                const windDirection = curData.windDirection;
+                const curWind = `${windSpeed} ${windDirection}`;
 
                 const weatherInfo = document.createElement('p');
                 weatherInfo.textContent = `${curShortForecast} ${curTemp}`;
@@ -23,8 +56,13 @@ class WeatherWidget extends HTMLElement {
                 const humidityInfo = document.createElement('p');
                 humidityInfo.textContent = `Humidity: ${curHumidity}`;
 
+                const windInfo = document.createElement('p');
+                windInfo.textContent = `Wind: ${curWind}`;
+
+                this.appendChild(weatherIcon);
                 this.appendChild(weatherInfo);
                 this.appendChild(humidityInfo);
+                this.appendChild(windInfo);
             })
             .catch((error) => {
                 console.error('Error fetching weather data:', error);
